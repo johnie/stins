d6b2d2d603154c72cd878598d0678b7bcd46dc33
Johnie Hjelm <johnie.hjelm@schibsted.com>
2026-02-02 15:20:42 +0100

    feat(middleware): add middleware barrel exports

---
+++ src/middleware/adapters/index.ts
@@ -0,0 +1,5 @@
+export * as elysiaMiddleware from "./elysia";
+export * as expressMiddleware from "./express";
+export * as h3Middleware from "./h3";
+export * as honoMiddleware from "./hono";
+export * as tanstackStartMiddleware from "./tanstack-start";
