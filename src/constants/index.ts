b3241e08951120393856f208d20b16e22c48645a
Johnie Hjelm <johnie.hjelm@schibsted.com>
2026-02-02 13:23:27 +0100

    feat(constants): add HTTP status codes and phrases
    Re-export status codes and phrases as namespaced modules.

---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+// biome-ignore lint/performance/noBarrelFile: Public API entry point
+export * as HttpStatusCodes from "./http-status-codes";
+export * as HttpStatusPhrases from "./http-status-phrases";
